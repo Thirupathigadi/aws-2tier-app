84aa96b2943528eeb2d27acdebcab5ad95c35ed9
Thirupathigadi <gadithirupathipatel@gmail.com>
2026-05-13 19:18:52 +0530

    Update rds.py

--- infrastructure/rds.py
+++ infrastructure/rds.py
@@ -28,19 +28,22 @@ def create_db_sg(vpc_id, web_sg_id):
 def create_rds(priv_subnet_ids, db_sg_id):
 
     rds = boto3.client('rds')
-    subnet_group_name = 'tier-db-subnet'
 
+    subnet_group_name = "tier-db-subnet"
+
+    # Create subnet group
     try:
         rds.create_db_subnet_group(
             DBSubnetGroupName=subnet_group_name,
             DBSubnetGroupDescription='RDS private subnets',
             SubnetIds=priv_subnet_ids
         )
-        print("Subnet group created")
+        print("DB subnet group created")
 
     except rds.exceptions.DBSubnetGroupAlreadyExistsFault:
-        print("Subnet group already exists")
+        print("DB subnet group already exists")
 
+    # Create DB instance
     rds.create_db_instance(
         DBInstanceIdentifier='tier-db',
         DBInstanceClass='db.t3.micro',
@@ -55,7 +58,7 @@ def create_rds(priv_subnet_ids, db_sg_id):
         PubliclyAccessible=False
     )
 
-    print("RDS creation started")
+    print("RDS creating... wait 5–10 mins")
 
     waiter = rds.get_waiter('db_instance_available')
     waiter.wait(DBInstanceIdentifier='2tier-db')
