9ed66ab3a30647ad30aa6ff35ccbb8727c9a695f
Thirupathigadi <gadithirupathipatel@gmail.com>
2026-05-13 19:19:22 +0530

    Update vpc.py

--- infrastructure/vpc.py
+++ infrastructure/vpc.py
@@ -3,7 +3,7 @@ import boto3
 def create_vpc_and_subnets():
     ec2 = boto3.client('ec2')
 
-    # ---------------- VPC ----------------
+    # Create VPC
     vpc = ec2.create_vpc(CidrBlock='10.0.0.0/16')
     vpc_id = vpc['Vpc']['VpcId']
 
@@ -19,22 +19,17 @@ def create_vpc_and_subnets():
 
     print(f"VPC created: {vpc_id}")
 
-    # ---------------- IGW ----------------
+    # Internet Gateway
     igw = ec2.create_internet_gateway()
     igw_id = igw['InternetGateway']['InternetGatewayId']
+    ec2.attach_internet_gateway(InternetGatewayId=igw_id, VpcId=vpc_id)
 
-    ec2.attach_internet_gateway(
-        InternetGatewayId=igw_id,
-        VpcId=vpc_id
-    )
-
-    # ---------------- PUBLIC SUBNET ----------------
+    # Public Subnet
     pub_subnet = ec2.create_subnet(
         VpcId=vpc_id,
         CidrBlock='10.0.1.0/24',
         AvailabilityZone='ap-south-1a'
     )
-
     pub_subnet_id = pub_subnet['Subnet']['SubnetId']
 
     ec2.modify_subnet_attribute(
@@ -42,20 +37,21 @@ def create_vpc_and_subnets():
         MapPublicIpOnLaunch={'Value': True}
     )
 
-    # ---------------- PRIVATE SUBNETS (IMPORTANT FIX) ----------------
+    # Private Subnet 1
     priv_subnet_1 = ec2.create_subnet(
         VpcId=vpc_id,
         CidrBlock='10.0.2.0/24',
         AvailabilityZone='ap-south-1a'
     )['Subnet']['SubnetId']
 
+    # Private Subnet 2 (IMPORTANT FOR RDS)
     priv_subnet_2 = ec2.create_subnet(
         VpcId=vpc_id,
         CidrBlock='10.0.3.0/24',
         AvailabilityZone='ap-south-1b'
     )['Subnet']['SubnetId']
 
-    # ---------------- ROUTE TABLE ----------------
+    # Route Table
     rt = ec2.create_route_table(VpcId=vpc_id)
     rt_id = rt['RouteTable']['RouteTableId']
 
@@ -67,7 +63,7 @@ def create_vpc_and_subnets():
 
     ec2.associate_route_table(RouteTableId=rt_id, SubnetId=pub_subnet_id)
 
-    print(f"Public subnet: {pub_subnet_id}")
-    print(f"Private subnets: {priv_subnet_1}, {priv_subnet_2}")
+    print(f"Public: {pub_subnet_id}")
+    print(f"Private: {[priv_subnet_1, priv_subnet_2]}")
 
     return vpc_id, pub_subnet_id, [priv_subnet_1, priv_subnet_2]
