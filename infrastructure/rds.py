91f997377cbbad29814b6658d1e61a10c575dad3
Thirupathigadi <gadithirupathipatel@gmail.com>
2026-05-13 19:10:38 +0530

    Update rds.py

--- infrastructure/rds.py
+++ infrastructure/rds.py
@@ -1,8 +1,5 @@
 import boto3
 
-# ----------------------------
-# SECURITY GROUP FOR DB
-# ----------------------------
 def create_db_sg(vpc_id, web_sg_id):
     ec2 = boto3.client('ec2')
 
@@ -27,51 +24,45 @@ def create_db_sg(vpc_id, web_sg_id):
     print(f"DB SG created: {sg_id}")
     return sg_id
 
-# ----------------------------
-# RDS CREATION
-# ----------------------------
+
 def create_rds(priv_subnet_ids, db_sg_id):
 
     rds = boto3.client('rds')
     subnet_group_name = '2tier-db-subnet-v2'
 
-    rds.create_db_subnet_group(
-        DBSubnetGroupName=subnet_group_name,
-        DBSubnetGroupDescription='RDS private subnets',
-        SubnetIds=priv_subnet_ids
-    )
-        print("DB Subnet Group created")
+    try:
+        rds.create_db_subnet_group(
+            DBSubnetGroupName=subnet_group_name,
+            DBSubnetGroupDescription='RDS private subnets',
+            SubnetIds=priv_subnet_ids
+        )
+        print("Subnet group created")
 
     except rds.exceptions.DBSubnetGroupAlreadyExistsFault:
-        print("DB Subnet Group already exists")
+        print("Subnet group already exists")
 
-    # Create RDS instance
-    try:
-        rds.create_db_instance(
-            DBInstanceIdentifier='2tier-db',
-            DBInstanceClass='db.t3.micro',
-            Engine='mysql',
-            MasterUsername='admin',
-            MasterUserPassword='Admin1234!123',
-            DBName='appdb',
-            AllocatedStorage=20,
-            VpcSecurityGroupIds=[db_sg_id],
-            DBSubnetGroupName=subnet_group_name,
-            MultiAZ=False,
-            PubliclyAccessible=False,
-            Tags=[{'Key': 'Name', 'Value': '2tier-rds'}]
-        )
-        print("RDS creation started")
+    rds.create_db_instance(
+        DBInstanceIdentifier='2tier-db',
+        DBInstanceClass='db.t3.micro',
+        Engine='mysql',
+        MasterUsername='admin',
+        MasterUserPassword='Admin1234!123',
+        DBName='appdb',
+        AllocatedStorage=20,
+        VpcSecurityGroupIds=[db_sg_id],
+        DBSubnetGroupName=subnet_group_name,
+        MultiAZ=False,
+        PubliclyAccessible=False
+    )
 
-    except rds.exceptions.DBInstanceAlreadyExistsFault:
-        print("RDS already exists")
+    print("RDS creation started")
 
-    # Wait for DB
     waiter = rds.get_waiter('db_instance_available')
     waiter.wait(DBInstanceIdentifier='2tier-db')
 
-    info = rds.describe_db_instances(DBInstanceIdentifier='2tier-db')
-    endpoint = info['DBInstances'][0]['Endpoint']['Address']
+    endpoint = rds.describe_db_instances(
+        DBInstanceIdentifier='2tier-db'
+    )['DBInstances'][0]['Endpoint']['Address']
 
     print(f"RDS endpoint: {endpoint}")
     return endpoint
